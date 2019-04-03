9171ebc1fe7e7c0cee1d94f1f31e8f149139077b
Parallels <ankit.anand.95@gmail.com>
2019-04-04 00:00:34 +0530

    Updated Scan fuction to scan krita layer

--- client/ClientLayerImage.py
+++ client/ClientLayerImage.py
@@ -1,5 +1,6 @@
 from sync.images import LayerImage
 from sync.images import register_image_class
+from .utils import *
 
 def get_node_object(layer_name):
     pass
@@ -13,7 +14,12 @@ class ClientLayerImage(LayerImage):
 
     def scan(self):
         # Scan Krita for updates
-        pass
+
+        activeDocument = getActiveDocument()
+        childNode = getChildNodes(activeDocument)
+        imageData = grabImage(childNode, 0, 0, 400, 400)
+        imageDataNumpyFormat = get_image_to_numpy(imageData)
+        LayerImage().send_updates(imageDataNumpyFormat)
 
     def handle_update(self, tile_key, data):
         # Write to krita
