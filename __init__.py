420fb8a823d8051092195e1b2be828af03fe8809
Parallels <ankit.anand.95@gmail.com>
2019-04-10 23:34:59 +0530

    Add path manipulation

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.append("/usr/local/lib/python3.5/site-packages")
+
 import krita
 
 from .widgets import CelestiaPrimeExtension
 import asyncio
+
 
 krita_instance = krita.Krita.instance()
 krita_instance.addExtension(CelestiaPrimeExtension(krita_instance))
