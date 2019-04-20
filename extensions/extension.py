81df09af84daa3e4389e08e75b5eaabbcc2d3152
Parallels <ankit.anand.95@gmail.com>
2019-04-20 21:02:59 +0530

    Added friggin ugly as fuck dialog box

--- extensions/extension.py
+++ extensions/extension.py
@@ -1,6 +1,7 @@
 from .dialog import PythonReferenceDialog
 import krita
-from sync.client import ClientLayerImage, ClientComputedImage
+from krita import QByteArray
+# from sync.client import ClientLayerImage, ClientComputedImage, utils
 
 class ClientExtension(krita.Extension):
     def __init__(self, parent, client):
@@ -16,36 +17,39 @@ class ClientExtension(krita.Extension):
                                      'tools/scripts')
         action.triggered.connect(self.nst_plugin)
 
-        action = window.createAction('demo', 'Demo', 'tools/scripts')
-        action.triggered.connect(self.demo)
+        # action = window.createAction('demo', 'Demo', 'tools/scripts')
+        # action.triggered.connect(self.demo)
 
     def nst_plugin(self):
         dlg = PythonReferenceDialog(
-            parent=self.parent.activeWindow().qwindow())
+            parent=self.parent.activeWindow().qwindow(), client=self.client)
         dlg.show()
         dlg.activateWindow()
 
-    def demo(self):
-        print("Setting up images...")
-        content = ClientLayerImage(
-            self.client.data_manager, {
-                "layer_name": "content",
-                "x0": 0,
-                "y0": 0,
-                "x_count": 5,
-                "y_count": 10,
-                "w": 100,
-            })
-        style = ClientLayerImage(
-            self.client.data_manager, {
-                "layer_name": "style",
-                "x0": 100,
-                "y0": 1000,
-                "x_count": 10,
-                "y_count": 10,
-                "w": 200
-            })
-        self.client.run_coroutine(content.register_self())
-        print(self.client.data_manager)
-        print(self.client.data_manager.images)
-        print("Done! Images should auto-sync now")
+    # def demo(self):
+    #     print("Setting up images...")
+    #     content_node = utils.get_node_object("content")
+    #     style_node = utils.get_node_object("style")
+
+    #     content = ClientLayerImage(
+    #         self.client.data_manager, {
+    #             "layer_name": content_node.name(),
+    #             "x0": 0,
+    #             "y0": 0,
+    #             "x_count": 5,
+    #             "y_count": 10,
+    #             "w": 100,
+    #         })
+    #     style = ClientLayerImage(
+    #         self.client.data_manager, {
+    #             "layer_name": style_node.name(),
+    #             "x0": 100,
+    #             "y0": 1000,
+    #             "x_count": 10,
+    #             "y_count": 10,
+    #             "w": 200
+    #         })
+    #     self.client.run_coroutine(content.register_self())
+    #     print(self.client.data_manager)
+    #     print(self.client.data_manager.images)
+    #     print("Done! Images should auto-sync now")
